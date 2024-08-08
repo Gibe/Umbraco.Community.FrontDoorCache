a6de2e4ebc49a802e65422117a8a321f897b0937
Steve Temple <steve@gibedigital.com>
2024-08-08 12:43:33 +0100

    Stop error when publishing media folders and draft items

--- src/Umbraco.Community.FrontDoorCache/ContentSavedNotificationHandler.cs
+++ src/Umbraco.Community.FrontDoorCache/ContentSavedNotificationHandler.cs
@@ -14,134 +14,139 @@ using Umbraco.Extensions;
 namespace Umbraco.Community.FrontDoorCache
 {
     public class ContentSavedNotificationHandler
-			: INotificationAsyncHandler<ContentSavedNotification>
-	{
-		private readonly ILogger _logger;
-		private readonly FrontDoorCacheOptions _options;
-		private readonly IFrontDoorApiClient _frontDoorApiClient;
-		private readonly IPublishedUrlProvider _publishedUrlProvider;
-		private readonly IUmbracoContextFactory _umbracoContextFactory;
+      : INotificationAsyncHandler<ContentSavedNotification>
+    {
+        private readonly ILogger _logger;
+        private readonly FrontDoorCacheOptions _options;
+        private readonly IFrontDoorApiClient _frontDoorApiClient;
+        private readonly IPublishedUrlProvider _publishedUrlProvider;
+        private readonly IUmbracoContextFactory _umbracoContextFactory;
 
-		public ContentSavedNotificationHandler(
-		IOptions<FrontDoorCacheOptions> options,
-		IUmbracoContextFactory umbracoContextFactory,
-		IPublishedUrlProvider publishedUrlProvider,
-		IFrontDoorApiClient frontDoorApiClient,
-		ILogger<MediaSavedNotificationHandler> logger)
-		{
-			_options = options.Value;
-			_frontDoorApiClient = frontDoorApiClient;
-			_umbracoContextFactory = umbracoContextFactory;
-			_publishedUrlProvider = publishedUrlProvider;
-			_logger = logger;
-		}
+        public ContentSavedNotificationHandler(
+        IOptions<FrontDoorCacheOptions> options,
+        IUmbracoContextFactory umbracoContextFactory,
+        IPublishedUrlProvider publishedUrlProvider,
+        IFrontDoorApiClient frontDoorApiClient,
+        ILogger<MediaSavedNotificationHandler> logger)
+        {
+            _options = options.Value;
+            _frontDoorApiClient = frontDoorApiClient;
+            _umbracoContextFactory = umbracoContextFactory;
+            _publishedUrlProvider = publishedUrlProvider;
+            _logger = logger;
+        }
 
-		public async Task HandleAsync(ContentSavedNotification notification, CancellationToken cancellationToken)
-		{
-			if (!_options.Enabled)
-			{
-				_logger.LogInformation("FrontDoor cache purging is disabled");
-				return;
-			}
+        public async Task HandleAsync(ContentSavedNotification notification, CancellationToken cancellationToken)
+        {
+            if (!_options.Enabled)
+            {
+                _logger.LogInformation("FrontDoor cache purging is disabled");
+                return;
+            }
 
-			switch (_options.Mode)
-			{
-				case PurgeCacheMode.All:
-					{
-						await PurgeAll();
-						return;
-					}
-				case PurgeCacheMode.Self:
-					{
-						await PurgeContent(notification.SavedEntities);
-						return;
-					}
-				case PurgeCacheMode.SelfAndAncestors:
-					{
-						await PurgeContent(notification.SavedEntities, true);
-						return;
-					}
-				case PurgeCacheMode.Unknown:
-					_logger.LogError("FrontDoor cache mode is unknown");
-					return;
-				default:
-					throw new ArgumentOutOfRangeException();
-			}
-		}
+            switch (_options.Mode)
+            {
+                case PurgeCacheMode.All:
+                {
+                    await PurgeAll();
+                    return;
+                }
+                case PurgeCacheMode.Self:
+                {
+                    await PurgeContent(notification.SavedEntities);
+                    return;
+                }
+                case PurgeCacheMode.SelfAndAncestors:
+                {
+                    await PurgeContent(notification.SavedEntities, true);
+                    return;
+                }
+                case PurgeCacheMode.Unknown:
+                    _logger.LogError("FrontDoor cache mode is unknown");
+                    return;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
 
-		private async Task PurgeContent(IEnumerable<IContent> savedContent, bool includeAncestors = false)
-		{
-			using var ctx = _umbracoContextFactory.EnsureUmbracoContext();
-			var contentCache = ctx.UmbracoContext.Content;
-			if (contentCache == null)
-			{
-				_logger.LogError("Failed to get published content cache");
-				return;
-			}
+        private async Task PurgeContent(IEnumerable<IContent> savedContent, bool includeAncestors = false)
+        {
+            using var ctx = _umbracoContextFactory.EnsureUmbracoContext();
+            var contentCache = ctx.UmbracoContext.Content;
+            if (contentCache == null)
+            {
+                _logger.LogError("Failed to get published content cache");
+                return;
+            }
 
-			var publishedContent = new List<IPublishedContent>();
-			foreach (var saved in savedContent)
-			{
-				var content = contentCache.GetById(saved.Id);
-				if (content == null)
-				{
-					_logger.LogWarning("Failed to get published content with id {Id}", saved.Id);
-					continue;
-				}
-				publishedContent.Add(content);
-				if (includeAncestors)
-				{
-					while (content.Parent != null)
-					{
-						if (publishedContent.Any(c => c.Id == content.Parent.Id))
-						{
-							break;
-						}
+            var publishedContent = new List<IPublishedContent>();
+            foreach (var saved in savedContent)
+            {
+                var content = contentCache.GetById(saved.Id);
+                if (content == null)
+                {
+                    _logger.LogWarning("Failed to get published content with id {Id}", saved.Id);
+                    continue;
+                }
 
-						content = content.Parent;
-						publishedContent.Add(content);
-					}
-				}
-			}
+                if (content.IsDraft())
+                {
+                    continue;
+                }
+                publishedContent.Add(content);
+                if (includeAncestors)
+                {
+                    while (content.Parent != null)
+                    {
+                        if (publishedContent.Any(c => c.Id == content.Parent.Id))
+                        {
+                            break;
+                        }
 
-			var request = GetContentPathsToPurge(publishedContent);
-			var paths = String.Join(", ", request.ContentPaths);
-			_logger.LogInformation("Purging FrontDoor cache {Mode}, clearing paths {paths}", _options.Mode, paths);
-			await _frontDoorApiClient.SendPurgeRequest(request);
-		}
+                        content = content.Parent;
+                        publishedContent.Add(content);
+                    }
+                }
+            }
 
-		private async Task PurgeAll()
-		{
-			_logger.LogInformation("Purging all FrontDoor cache");
-			var result = await _frontDoorApiClient.SendPurgeAllRequest();
-			if (!result)
-			{
-				_logger.LogError("Failed to purge FrontDoor cache");
-			}
-			else
-			{
-				_logger.LogInformation("Successfully purged FrontDoor cache");
-			}
-		}
+            var request = GetContentPathsToPurge(publishedContent);
+            var paths = string.Join(", ", request.ContentPaths);
+            _logger.LogInformation("Purging FrontDoor cache {Mode}, clearing paths {paths}", _options.Mode, paths);
+            await _frontDoorApiClient.SendPurgeRequest(request);
+        }
 
-		private FrontDoorPurgeContent GetContentPathsToPurge(List<IPublishedContent> publishedContent)
-		{
-			var contentPaths = new List<string>();
+        private async Task PurgeAll()
+        {
+            _logger.LogInformation("Purging all FrontDoor cache");
+            var result = await _frontDoorApiClient.SendPurgeAllRequest();
+            if (!result)
+            {
+                _logger.LogError("Failed to purge FrontDoor cache");
+            }
+            else
+            {
+                _logger.LogInformation("Successfully purged FrontDoor cache");
+            }
+        }
 
-			foreach (var content in publishedContent)
-			{
-				foreach (var culture in content.Cultures)
-				{
-					var url = content.Url(_publishedUrlProvider, culture.Key, UrlMode.Absolute);
-					var uri = new UriBuilder(url);
-					contentPaths.Add(uri.Path);
-				}
-			}
+        private FrontDoorPurgeContent GetContentPathsToPurge(List<IPublishedContent> publishedContent)
+        {
+            var contentPaths = new List<string>();
 
-			return new FrontDoorPurgeContent(contentPaths);
-		}
+            foreach (var content in publishedContent)
+            {
+                foreach (var culture in content.Cultures)
+                {
+                    var url = content.Url(_publishedUrlProvider, culture.Key, UrlMode.Absolute);
+                    var uri = new UriBuilder(url);
+                    contentPaths.Add(uri.Path);
+                }
+            }
+
+            return new FrontDoorPurgeContent(contentPaths);
+        }
 
 
-	}
+    }
 
 }
